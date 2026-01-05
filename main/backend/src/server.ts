7c52d04436bb8c176122169dc3684cc9b4a93039
Deeksha Nigoti <nigotideeksha@gmqail.com>
2026-01-05 20:17:05 +0530

    added attack simulator

--- main/backend/src/server.ts
+++ main/backend/src/server.ts
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv';
+import { startAttackSimulator } from "./services/attackSimulator";
+
 dotenv.config();
 
 import app from './app';
@@ -11,3 +13,6 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+startAttackSimulator(5000); // every 5 seconds
+
